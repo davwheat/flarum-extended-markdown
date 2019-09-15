675f21200ee96037d23975b079ffaadbf341b474
David Wheatley <blokmaster.help@gmail.com>
2019-09-15 17:33:13 +0100

    Fix indentation

--- extend.php
+++ extend.php
@@ -15,36 +15,36 @@ use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
 
 return [
-	(new Extend\Formatter)
-		->configure(function (Configurator $config) {
-			$config->BBcodes->addCustom(
-				'[SUPER]{TEXT}[/SUPER]',
-				'<sup>{TEXT}</sup>'
-			);
-		}),
-	(new Extend\Formatter)
-		->configure(function (Configurator $config) {
-			$config->BBcodes->addCustom(
-				'[SUB]{TEXT}[/SUB]',
-				'<sub>{TEXT}</sub>'
-			);
-		}),
-	(new Extend\Formatter)
-		->configure(function (Configurator $config) {
-			$config->BBcodes->addCustom(
-				'[Z]{TEXT}',
-				'<label class="extendedmarkdown--checkbox"><input type="checkbox"  class="extendedmarkdown--checkbox-input" disabled/> {TEXT}</label>'
-			);
-		}),
-	(new Extend\Formatter)
-		->configure(function (Configurator $config) {
-			$config->BBcodes->addCustom(
-				'[X]{TEXT}',
-				'<label class="extendedmarkdown--checkbox"><input type="checkbox"  class="extendedmarkdown--checkbox-input" disabled checked/>{TEXT}</label>'
-			);
-		}),
-	(new Extend\Frontend('forum'))
-		->js(__DIR__ . '/js/dist/forum.js')
-		->css(__DIR__ . '/resources/less/forum.less')
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBcodes->addCustom(
+                '[SUPER]{TEXT}[/SUPER]',
+                '<sup>{TEXT}</sup>'
+            );
+        }),
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBcodes->addCustom(
+                '[SUB]{TEXT}[/SUB]',
+                '<sub>{TEXT}</sub>'
+            );
+        }),
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBcodes->addCustom(
+                '[Z]{TEXT}',
+                '<label class="extendedmarkdown--checkbox"><input type="checkbox"  class="extendedmarkdown--checkbox-input" disabled/> {TEXT}</label>'
+            );
+        }),
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBcodes->addCustom(
+                '[X]{TEXT}',
+                '<label class="extendedmarkdown--checkbox"><input type="checkbox"  class="extendedmarkdown--checkbox-input" disabled checked/>{TEXT}</label>'
+            );
+        }),
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/resources/less/forum.less')
 
 ];
